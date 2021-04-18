275e2f7394f1fd0e2d6b9c617af428856c159f90
Gus Gita Pradnyana <wildbot110@gmail.com>
2021-04-18 14:52:17 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import Home from "./components/Home";
+import Details from "./components/Details";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [loaded] = useFonts({
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+  });
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          r
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
